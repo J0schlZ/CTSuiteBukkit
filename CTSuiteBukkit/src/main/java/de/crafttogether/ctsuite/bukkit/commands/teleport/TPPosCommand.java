8d3e5a7d13ef9a4635222e49118ae153c9d69fd3
J0schlZ <j0schlz@craft-together.de>
2018-11-26 16:30:18 +0100

    Working on a teleportation system

--- CTSuiteBukkit/src/main/java/de/crafttogether/ctsuite/bukkit/commands/teleport/TPPosCommand.java
+++ CTSuiteBukkit/src/main/java/de/crafttogether/ctsuite/bukkit/commands/teleport/TPPosCommand.java
@@ -1,9 +1,13 @@
 package de.crafttogether.ctsuite.bukkit.commands.teleport;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -22,11 +26,14 @@ public class TPPosCommand implements TabExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String st, String[] args) {
         Player p = null;
         Player target = null;
+        World world = null;
+        String worldName = p.getWorld().getName();
+        String serverName = p.getServer().getName();
         
         if (sender instanceof Player)
             p = Bukkit.getPlayer(((Player) sender).getUniqueId());
 
-        if (args.length != 1)
+        if (args.length != 3 || !args[0].matches("[0-9.]+") || !args[1].matches("[0-9.]+") || !args[2].matches("[0-9.]+"))
         	return false;
         
         if (p == null) {
@@ -36,15 +43,35 @@ public class TPPosCommand implements TabExecutor {
         
         if (!this.main.getPlayerHandler().checkPermission(p, "ctsuite.command.tppos"))
             return true;
+
+        if (args[4] != null)
+        	serverName = args[4];
         
-        target = Bukkit.getPlayer(args[0]);
-     
-        if (target != null && target.isOnline())
-            p.teleport(target.getLocation());
+        if (args[3] != null)
+        	worldName = args[3];
+        
+        String x = args[0];
+        String y = args[1];
+        String z = args[2];
+        String pitch = (!args[5].matches("[0-9.]+")) ? null : args[5];
+        String yaw = (!args[6].matches("[0-9.]+")) ? null : args[6];
+        
+        String strLocation = serverName + ":" + worldName + ":" + x + ":" + y + ":" + z + ":" + pitch + ":" + yaw;        
+        world = Bukkit.getWorld(worldName);
+        
+        // Location is on same Server
+        if (world != null && p.isOnline() && p.getServer().getName().equalsIgnoreCase(serverName)) {
+        	Location loc = new Location(world, Double.parseDouble(x), Double.parseDouble(y), Double.parseDouble(z));
+        	
+        	if (pitch != null)	loc.setPitch(Float.parseFloat(pitch));
+        	if (yaw != null)	loc.setYaw(Float.parseFloat(yaw));
+        	
+        	p.teleport(loc);
+        }
         else {
         	PMessage pm = new PMessage(this.main, "bungee.player.cmd.tppos");
-            pm.put(p.getName());
-            pm.put(args[0]);
+            pm.put(p.getUniqueId().toString());
+            pm.put(strLocation);
             pm.put("true");
             pm.send(p);
         }
@@ -54,6 +81,40 @@ public class TPPosCommand implements TabExecutor {
     
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {		
+		if (cmd.getName().equalsIgnoreCase("gamemode") || cmd.getName().equalsIgnoreCase("gm")) {
+			Player p = null;
+			List<String> proposals = new ArrayList<String>();
+			List<String> newList = new ArrayList<String>();
+			
+			if (sender instanceof Player)
+				p = (Player) sender;
+			
+			if (p == null || main.getPlayerHandler().hasPermission(p, "ctsuite.command.tppos"))
+				return null;
+
+			switch (args.length) {
+				case 1: proposals.add(""+ p.getLocation().getX()); break;
+				case 2: proposals.add(""+ p.getLocation().getY()); break;
+				case 3: proposals.add(""+ p.getLocation().getZ()); break;
+				case 4: proposals.add(""+ p.getWorld().getName()); break;
+				case 5: proposals.add(""+ p.getServer().getName()); break;
+				case 6: proposals.add(""+ p.getLocation().getYaw()); break;
+				case 7: proposals.add(""+ p.getLocation().getPitch()); break;
+			}
+
+			if (args[args.length -1].equals(""))
+				newList = proposals;
+			else {
+				for (String value : proposals) {
+					if (value.toLowerCase().startsWith(args[args.length -1].toLowerCase())) {
+						newList.add(value);
+					}
+				}
+			}
+			
+			return newList;
+		}
+		
 		return null;
 	}
 }
